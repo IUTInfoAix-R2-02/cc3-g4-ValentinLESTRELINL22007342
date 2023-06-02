6f1910c1504dd9fbb3b2019e8b364e8bf319fe83
Valentin Lestrelin <valentin.lestrelin@etu.univ-amu.fr>
2023-06-02 14:29:44 +0200

    FXml + début point

--- src/main/java/fr/amu/iut/cc3/ToileApp.java
+++ src/main/java/fr/amu/iut/cc3/ToileApp.java
@@ -1,9 +1,12 @@
 package fr.amu.iut.cc3;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -13,8 +16,10 @@ public class ToileApp extends Application {
     @Override
     public void start(Stage primaryStage) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("toile.fxml"));
+
         primaryStage.setResizable(false);
         primaryStage.setScene( new Scene(root) );
+        primaryStage.setTitle("Résultats aux différentes compétences du BUT");
         primaryStage.show();
     }
 
--- src/main/java/fr/amu/iut/cc3/ToileController.java
+++ src/main/java/fr/amu/iut/cc3/ToileController.java
@@ -10,13 +10,19 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Point2D;
 import javafx.scene.Node;
+import javafx.scene.PointLight;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.effect.Light;
+import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
 
 import java.net.URL;
@@ -31,9 +37,17 @@ public class ToileController implements Initializable {
     private static int angleDepart = 90;
     private static int noteMaximale = 20;
 
+    @FXML
+    Pane radar;
+    @FXML
+    TextField comp1 = new TextField();
+    @FXML
+    TextField comp2;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        comp1.addEventHandler(KeyEvent.KEY_PRESSED,
+                keyEvent -> pointInRadar(keyEvent, 1, comp1.getText()));
     }
 
     int getXRadarChart(double value, int axe ){
@@ -44,6 +58,21 @@ public class ToileController implements Initializable {
     int getYRadarChart(double value, int axe ){
         return (int) (rayonCercleExterieur - Math.sin(Math.toRadians(angleDepart - (axe-1)  * angleEnDegre)) * rayonCercleExterieur
                 *  (value / noteMaximale));
+    }
+
+    private void pointInRadar(KeyEvent event, int competence, String number){
+        if (event.getCode().equals(KeyCode.ENTER)) {
+            if (number.matches("[0-9]+")) {
+                double nb = Integer.valueOf(number);
+                if (nb >= 0 && nb <= 20){
+                    Circle pointGraphe = new Circle();
+                    pointGraphe.setCenterY(getYRadarChart(nb, competence));
+                    pointGraphe.setCenterX(getXRadarChart(nb, competence));
+                    pointGraphe.setStyle("-fx-background-color: black");
+                    pointGraphe.setRadius(10.00);
+                }
+            }
+        }
     }
 
 }
